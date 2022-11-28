97bae58952d06ef11e48e85f08d6c3d92fa2187e
Alan <alannhan@ymail.com>
2022-11-28 13:49:40 -0800

    Added edit functionality but doesn't redirect page to edited page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const router = require("./controllers/places");
 const methodOverride = require("method-override");
 
 // Express Settings
@@ -11,6 +12,7 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use("/places", router);
 
 // Controllers & Routes
 app.use("/places", require("./controllers/places"));
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,12 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+              <p>Not Rated</p>
+            <h2>Description</h2>
+              <p>No Description</p>
+            <h2>Comments</h2>
+              <p>No Comments</p>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
               </a>     
